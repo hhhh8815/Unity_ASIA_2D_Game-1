4f2ad108e5b07f6866f09ae80d598a6f858baac5
hhhh8815 <55180599+hhhh8815@users.noreply.github.com>
2020-11-03 16:05:24 +0800

    偵測碰撞

--- 2DGame/Assets/Scripts/MenuManager.cs
+++ 2DGame/Assets/Scripts/MenuManager.cs
@@ -1,15 +1,19 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    void StartGame()
+    public void StartGame()
     {
-        
+        print("遊戲開始");
+
+        SceneManager.LoadScene("關卡1");  // 場景管理.載入場景("名稱")
+        //SceneManager.LoadScene(1);      // 場景管理.載入場景(編號)
     }
 
-    void QuitGame()
+    public void QuitGame()
     {
-
+        print("離開遊戲");
+        Application.Quit();
     }
 }
---
+++ 2DGame/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject final;
+
+    // 碰撞偵測條件
+    // 1. 兩個物件必須有碰撞器 Collider 2D
+    // 2. 兩者必須至少有一個剛體 Rigidbody2D
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("碰到傳送門惹!");
+        final.SetActive(true);  //
+    }
+}
